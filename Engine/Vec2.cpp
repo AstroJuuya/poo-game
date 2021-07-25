7a1a196109f064455632ce9902b80d104744f572
Juuya <Stridin@protonmail.com>
2021-07-25 18:11:33 +0200

    Added more vector operations

--- Engine/Vec2.cpp
+++ Engine/Vec2.cpp
@@ -1,4 +1,5 @@
 #include "Vec2.h"
+#include <cmath>
 
 Vec2::Vec2(float x_in, float y_in)
 	:
@@ -17,6 +18,16 @@ Vec2& Vec2::operator+=(const Vec2& rhs)
 	return *this = *this + rhs;
 }
 
+Vec2 Vec2::operator-(const Vec2& rhs)
+{
+	return Vec2( x - rhs.x, y - rhs.y );
+}
+
+Vec2& Vec2::operator-=(const Vec2& rhs)
+{
+	return *this = *this - rhs;
+}
+
 Vec2 Vec2::operator*(const float rhs) const
 {
 	return Vec2( x * rhs, y * rhs );
@@ -25,4 +36,29 @@ Vec2 Vec2::operator*(const float rhs) const
 Vec2& Vec2::operator*=(const float rhs)
 {
 	return *this = *this * rhs;
+}
+
+float Vec2::GetLength() const
+{
+	return std::sqrt( GetLengthSq() );
+}
+
+float Vec2::GetLengthSq() const
+{
+	return x * x + y * y;
+}
+
+Vec2& Vec2::Normalize()
+{
+	return *this = GetNormalized();
+}
+
+Vec2 Vec2::GetNormalized() const
+{
+	const float len = GetLength();
+	if (len != 0.0f)
+	{
+		return *this * (1.0f / len);
+	}
+	return *this;
 }
