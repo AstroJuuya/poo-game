815c0515e6a1ca29878fb85ed8e02ffff8112a7d
Stridin <Stridin@protonmail.com>
2021-07-25 14:26:25 +0200

    Added FrameTimer and ft Mark call

--- Engine/Game.h
+++ Engine/Game.h
@@ -28,6 +28,7 @@
 #include "Goal.h"
 #include "Meter.h"
 #include "SoundEffect.h"
+#include "FrameTimer.h"
 #include <random>
 
 class Game
@@ -54,6 +55,7 @@ private:
 	std::mt19937 rng;
 	std::uniform_real_distribution<float> xDist;
 	std::uniform_real_distribution<float> yDist;
+	FrameTimer ft;
 	Dude dude;
 	Goal goal;
 	Meter meter;
